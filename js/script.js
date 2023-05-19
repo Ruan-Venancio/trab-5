cec21a0eb90747eab2add0be10992251bd6f30dd
ruan <venancioruan11@gmail.com>
2023-05-19 11:24:18 -0300

    longe do fim

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 function click_Menu(){
     var itens = document.querySelectorAll('.itens');
+    var hambuguer = document.getElementById("hamburger");
+    console.log(hambuguer);
 
-    console.log(itens);
+    for (i = 0; i < itens.length; ++i){
+        if (itens[i].style.display == "block"){
+            hambuguer.style.marginTop = "auto";
+            itens[i].style.display = "none";
+        }
+        else{
+            hambuguer.style.marginTop = "0";
+            itens[i].style.display = "block";
+        }
+        
+    }
 }
