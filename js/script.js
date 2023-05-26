71db321612c5fc049fab22273d1976564908ac93
ruan <venancioruan11@gmail.com>
2023-05-26 09:34:13 -0300

    quite carousel effect

--- js/script.js
+++ js/script.js
@@ -17,10 +17,40 @@ function click_Menu(){
 }
 
 function transition_imgs(){
-    let time = 2000
-    var images = document.querySelectorAll("#slide img")
-    console.log(images)
+
+    let time = 2000;
+    var images = document.querySelectorAll("#slide img");
+    var max = images.length-1;
+    var count = 0;
+    var crescente = 1
+
+    function next_image(){
+
+        if(crescente == 1){
+            console.log(max - count)
+            console.log(count)
+            images[max - count].classList.add("opacity");
+            count ++;
+            if(count == 3){
+                crescente = 0;
+            }
+        }
+        else{
+            images[max - count  + 1].classList.remove("opacity");
+            count --;
+            if(count == 0){
+                crescente = 1;
+            }
+        }
+    }
+    
+    function start(){
+        setInterval(() => {
+            next_image();
+        }, time);
+    }
+    window.addEventListener("load", start);
 
 }
 
-transition_imgs()
+transition_imgs();
